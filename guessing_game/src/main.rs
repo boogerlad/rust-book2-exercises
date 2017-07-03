3ff6a780e59e3e4d6fb497cb8006d2cecc2344f3
root <root@localhost.localdomain>
2017-07-03 14:04:29 -0400

    add guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main()
+{
+	//let wow = b'A';
+	//println!("{}", wow);
+	println!("guess the number!");
+	let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+	//println!("the secret number is {}", secret_number);
+	loop
+	{
+		println!("input your guess");
+		let mut guess = String::new();
+		io::stdin().read_line(&mut guess)
+			.expect("failed to read line");
+		let guess: u32 = match guess.trim().parse()//.expect("not a number");
+		{
+			Ok(num) => num,
+			Err(_) => continue
+		};
+		println!("you guessed {}", guess);
+		match guess.cmp(&secret_number)
+		{
+			Ordering::Less => println!("too small"),
+			Ordering::Equal => {
+				println!("correct!");
+				break;
+			},
+			Ordering::Greater => println!("too big")
+		}
+	}
+}
